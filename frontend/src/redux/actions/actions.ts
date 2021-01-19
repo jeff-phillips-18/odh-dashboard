d4c21d8d0d8f2940ce9193f14f5dc2bb22347ed2
Jeffrey Phillips <jephilli@redhat.com>
2021-01-19 14:24:53 -0500

    Update UI nav flow and views.

--- frontend/src/redux/actions/actions.ts
+++ frontend/src/redux/actions/actions.ts
@@ -1,4 +1,27 @@
+import * as _ from 'lodash';
 import ky from 'ky';
+
+export const GET_USER_PENDING = 'GET_USER_PENDING';
+export const getUserPending = () => ({
+  type: GET_USER_PENDING,
+  payload: {},
+});
+
+export const GET_USER_FULFILLED = 'GET_USER_FULFILLED';
+export const getUserFullfilled = (response) => ({
+  type: GET_USER_FULFILLED,
+  payload: {
+    user: response.kube.currentUser,
+  },
+});
+
+export const GET_USER_REJECTED = 'GET_USER_REJECTED';
+export const getUserRejected = (error) => ({
+  type: GET_USER_REJECTED,
+  payload: {
+    error,
+  },
+});
 
 export const GET_COMPONENTS_PENDING = 'GET_COMPONENTS_PENDING';
 export const getComponentsPending = () => ({
@@ -22,12 +45,17 @@ export const getComponentsRejected = (error) => ({
   },
 });
 
-export const getComponents = () => {
+export const getComponents = (installed: boolean = false) => {
   const url = `/api/components`;
   return async function (dispatch) {
     dispatch(getComponentsPending());
+    const searchParams = new URLSearchParams();
+    if (installed) {
+      searchParams.set('installed', 'true');
+    }
+    const options = { searchParams };
     try {
-      const response = await ky.get(url, {}).json();
+      const response = await ky.get(url, options).json();
       dispatch(getComponentsFulfilled(response));
     } catch (e) {
       let componentError;
@@ -42,4 +70,23 @@ export const getComponents = () => {
   };
 };
 
-export default getComponents;
+export const detectUser = () => {
+  const url = `${window.location.protocol}//${window.location.hostname}:9010/api/status`;
+  return async function (dispatch) {
+    dispatch(getUserPending());
+    try {
+      const response = await ky.get(url, {}).json();
+      dispatch(getUserFullfilled(response));
+    } catch (e) {
+      let userError;
+      console.dir(e);
+      if (e instanceof ky.HTTPError) {
+        userError = await e.response.json();
+      } else {
+        userError = e;
+      }
+      console.error(e);
+      dispatch(getUserRejected(userError));
+    }
+  };
+};
