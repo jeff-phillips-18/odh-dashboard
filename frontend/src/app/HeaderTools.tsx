3a1deb9f3cba106682b48e9def9303c2cff22f02
Jeffrey Phillips <jephilli@redhat.com>
2021-04-08 07:40:05 -0400

    test

--- frontend/src/app/HeaderTools.tsx
+++ frontend/src/app/HeaderTools.tsx
@@ -12,6 +12,35 @@ import { CaretDownIcon } from '@patternfly/react-icons';
 import { useHistory } from 'react-router';
 
 const AUTH_COOKIE = '_oauth_proxy';
+const TEST_COOKIE = 'sat_prevExtCmp';
+
+const createCookie = (name: string, value: string, days: number) => {
+  let expires;
+  if (days) {
+    const date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    expires = `; expires=${date.toUTCString()}`;
+  } else {
+    expires = '';
+  }
+
+  document.cookie = `${name}=${value}${expires}; path=/`;
+};
+
+const readCookie = (name: string) => {
+  const nameEQ = `${name}=`;
+  const ca = document.cookie.split(';');
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+};
+
+const eraseCookie = (name: string) => {
+  createCookie(name, '', -1);
+};
 
 type HeaderToolsProps = {
   user: { name: string; token: string };
@@ -23,13 +52,36 @@ const HeaderTools: React.FC<HeaderToolsProps> = ({ user }) => {
 
   const handleLogout = () => {
     document.cookie = `${AUTH_COOKIE}=;expires=Thu, 01 Jan 1970 00:00:01 GMT; path=/`;
+    document.cookie = `${TEST_COOKIE}=;expires=Thu, 01 Jan 1970 00:00:01 GMT; path=/`;
     setUserMenuOpen(false);
     history.push('/');
+  };
+  const handleShowCookies = () => {
+    console.dir(document.cookie);
+    console.log(readCookie('_jeffTestCookie'));
+    setUserMenuOpen(false);
+  };
+  const handleAddCookie = () => {
+    createCookie('_jeffTestCookie', 'Wtf_Wtf', 3);
+    setUserMenuOpen(false);
+  };
+  const handleDeleteCookie = () => {
+    eraseCookie('_jeffTestCookie');
+    setUserMenuOpen(false);
   };
 
   const userMenuItems = [
     <DropdownItem key="logout" onClick={handleLogout}>
       Log out
+    </DropdownItem>,
+    <DropdownItem key="cookies" onClick={handleShowCookies}>
+      Show Cookies
+    </DropdownItem>,
+    <DropdownItem key="add" onClick={handleAddCookie}>
+      Add Cookie
+    </DropdownItem>,
+    <DropdownItem key="delete" onClick={handleDeleteCookie}>
+      Delete Cookie
     </DropdownItem>,
   ];
   const userName = React.useMemo(() => {
