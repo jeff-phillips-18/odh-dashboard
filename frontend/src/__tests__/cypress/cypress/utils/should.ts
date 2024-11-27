4e450d5266af87c4b7c5c6e0aa1504200a2beda9
Eric Olkowski <thatblindgeye@gmail.com>
2024-11-27 13:31:16 -0500

    Fix lint errors

--- frontend/src/__tests__/cypress/cypress/utils/should.ts
+++ frontend/src/__tests__/cypress/cypress/utils/should.ts
@@ -1,8 +1,8 @@
 const status = {
   default: ($subject: JQuery<HTMLElement>) => {
     const classes = $subject.attr('class');
-    expect($subject.hasClass('pf-v6-c-helper-text__item')).to.eq(true) &&
-      classes?.split(' ').length === 1;
+    expect($subject.hasClass('pf-v6-c-helper-text__item')).to.eq(true);
+    expect(classes?.split(' ').length).to.eq(1);
   },
   warning: ($subject: JQuery<HTMLElement>) => {
     expect($subject.hasClass('pf-m-warning')).to.eq(true);
